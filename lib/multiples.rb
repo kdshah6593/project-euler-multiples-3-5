6ea6990ddcc711c99db97b125015b23e3468bb89
Kunal <kdshah6593@gmail.com>
2021-08-12 14:41:57 -0400

    completed solution, all tests passing

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    i = 1
+    multiples = []
+    while i < limit do
+        if i % 3 == 0 || i % 5 == 0
+            multiples << i
+        end
+        i += 1
+    end
+    multiples
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).inject {|sum, n| sum + n }
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,17 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+    attr_accessor :limit
+
+    def initialize(limit)
+        self.limit = limit
+    end
+
+    def collect_multiples
+        (1..self.limit - 1).find_all { |i| i % 3 == 0 || i % 5 == 0}
+    end
+
+    def sum_multiples
+        self.collect_multiples.reduce{|sum, n| sum + n}
+    end
+end
